31af1e3e39639fb28635d93a69c561f4fe75f767
resyahrial <syahrialrangga11@gmail.com>
2022-05-28 21:33:21 +0700

    implement validation on domain

--- internal/domains/authentication/authentication.go
+++ internal/domains/authentication/authentication.go
@@ -1,8 +1,14 @@
 package authentication
 
+import "github.com/resyahrial/go-commerce/pkg/gvalidator"
+
 type Login struct {
-	Email    string `json:"email"`
+	Email    string `json:"email" validate:"email"`
 	Password string `json:"-"`
+}
+
+func (l Login) Validate() (string, bool) {
+	return gvalidator.Validate(l)
 }
 
 type Token struct {
