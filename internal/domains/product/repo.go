cceff9cdd11829f0d357c9d64064044bc35406c7
resyahrial <syahrialrangga11@gmail.com>
2022-05-29 08:54:00 +0700

    add list method on product repo

--- internal/domains/product/repo.go
+++ internal/domains/product/repo.go
@@ -1,5 +1,19 @@
 package product
 
+import (
+	"context"
+
+	"github.com/segmentio/ksuid"
+)
+
 //go:generate mockgen -destination=mocks/mock.go -source=repo.go ProductRepo
 
-type ProductRepo interface{}
+type ProductRepo interface {
+	GetList(ctx context.Context, params GetListParams) ([]Product, error)
+}
+
+type GetListParams struct {
+	Page     int
+	Limit    int
+	SellerId ksuid.KSUID
+}
