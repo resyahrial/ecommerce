5f16ded10c5a29bf2315a8947643847b78dbbc1c
resyahrial <syahrialrangga11@gmail.com>
2022-05-29 14:52:46 +0700

    create product validation

--- internal/domains/product/product.go
+++ internal/domains/product/product.go
@@ -2,13 +2,18 @@ package product
 
 import (
 	"github.com/resyahrial/go-commerce/internal/domains/user"
+	"github.com/resyahrial/go-commerce/pkg/gvalidator"
 	"github.com/segmentio/ksuid"
 )
 
 type Product struct {
 	ID          ksuid.KSUID `json:"ID"`
-	Name        string      `json:"name"`
-	Description string      `json:"description"`
-	Price       float64     `json:"price"`
+	Name        string      `json:"name" validate:"required,max=50"`
+	Description string      `json:"description" validate:"required"`
+	Price       float64     `json:"price" validate:"required,gte=0"`
 	Seller      user.Seller `json:"seller"`
+}
+
+func (p Product) Validate() (string, bool) {
+	return gvalidator.Validate(p)
 }
