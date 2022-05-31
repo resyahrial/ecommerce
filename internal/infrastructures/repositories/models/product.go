da36c331eeee17f1d7c409b415e76d72dc652245
resyahrial <syahrialrangga11@gmail.com>
2022-05-31 21:38:59 +0700

    create product data model

---
+++ internal/infrastructures/repositories/models/product.go
@@ -0,0 +1,27 @@
+package models
+
+import (
+	"time"
+
+	"github.com/segmentio/ksuid"
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	ID          ksuid.KSUID `json:"ID" gorm:"primaryKey;type:varchar(30)"`
+	SellerId    ksuid.KSUID `json:"sellerId" gorm:"index;type:varchar(30)"`
+	InsertedAt  time.Time   `json:"insertedAt" gorm:"autoCreateTime"`
+	UpdatedAt   time.Time   `json:"updatedAt" gorm:"autoUpdateTime"`
+	IsDeleted   bool        `json:"isDeleted"`
+	Name        string      `json:"name"`
+	Description string      `json:"description"`
+	Price       float64     `json:"price"`
+	User        User        `json:"-" gorm:"foreignKey:UserId;references:SellerId;OnDelete:SET NULL" validate:"-"`
+}
+
+func (p *Product) BeforeCreate(tx *gorm.DB) (err error) {
+	if p.ID.IsNil() {
+		p.ID = ksuid.New()
+	}
+	return
+}
