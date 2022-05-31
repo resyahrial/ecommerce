bba80a903f5b81bb213f7a15fa7d5e2e5868885e
resyahrial <syahrialrangga11@gmail.com>
2022-05-31 22:07:42 +0700

    create get list product repo

--- internal/infrastructures/repositories/product/repo.go
+++ internal/infrastructures/repositories/product/repo.go
@@ -5,9 +5,10 @@ import (
 
 	"github.com/mitchellh/mapstructure"
 	product_dom "github.com/resyahrial/go-commerce/internal/domains/product"
+	user_dom "github.com/resyahrial/go-commerce/internal/domains/user"
 	"github.com/resyahrial/go-commerce/internal/infrastructures/repositories/models"
 	"github.com/resyahrial/go-commerce/pkg/gtrace"
-	"github.com/resyahrial/go-commerce/pkg/inspect"
+	"github.com/thoas/go-funk"
 	"gorm.io/gorm"
 )
 
@@ -19,11 +20,54 @@ func New(db *gorm.DB) product_dom.ProductRepo {
 	return &ProductRepoPg{db}
 }
 
-func (r *ProductRepoPg) GetList(ctx context.Context, params product_dom.GetListParams) (products []product_dom.Product, count int64, err error) {
+func (r *ProductRepoPg) GetList(ctx context.Context, params product_dom.GetListParams) (res []product_dom.Product, count int64, err error) {
 	newCtx, span := gtrace.Start(ctx)
 	defer gtrace.End(span, err)
 
-	inspect.Do(newCtx)
+	var products []models.Product
+
+	offset := params.Page * params.Limit
+	result := r.db.WithContext(newCtx)
+	queries := []string{}
+	values := []interface{}{}
+
+	if params.Ksuids != nil {
+		queries = append(queries, "ksuid IN ?")
+		values = append(values, params.Ksuids)
+	}
+
+	if !params.SellerId.IsNil() {
+		queries = append(queries, "seller_id = ?")
+		values = append(values, params.SellerId)
+	}
+
+	if params.PreloadSeller {
+		result = result.Preload("User")
+	}
+
+	joinedQuery := funk.Reduce(queries, func(acc string, query string) string {
+		return acc + query
+	}, "")
+
+	result = result.Model(&models.Product{}).Where(joinedQuery, values...)
+
+	if err = result.Count(&count).Error; err != nil {
+		return
+	}
+
+	result = result.Limit(params.Limit).Offset(offset)
+
+	if err = result.Find(&products).Error; err != nil {
+		return
+	}
+
+	for _, p := range products {
+		var productDomain product_dom.Product
+		if productDomain, err = r.castDataModelsToDomain(newCtx, p); err != nil {
+			return
+		}
+		res = append(res, productDomain)
+	}
 
 	return
 }
@@ -43,6 +87,26 @@ func (r *ProductRepoPg) Create(ctx context.Context, input product_dom.Product) (
 
 	if err = mapstructure.Decode(dataProduct, &res); err != nil {
 		return
+	}
+
+	return
+}
+
+func (r *ProductRepoPg) castDataModelsToDomain(ctx context.Context, p models.Product) (pDom product_dom.Product, err error) {
+	_, span := gtrace.Start(ctx)
+	defer gtrace.End(span, err)
+
+	var userDomain user_dom.User
+	if err = mapstructure.Decode(p, &pDom); err != nil {
+		return
+	}
+
+	if err = mapstructure.Decode(p.User, &userDomain); err != nil {
+		return
+	}
+
+	if seller, ok := userDomain.ToSeller(); ok {
+		pDom.Seller = seller
 	}
 
 	return
