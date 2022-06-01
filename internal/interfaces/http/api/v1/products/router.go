9f339b05ee8572ea4d7f0a8c848945356e1c3639
Rangga Eki Syahrial <58381367+resyahrial@users.noreply.github.com>
2022-06-01 19:25:22 +0700

    Product handler (#17)
    * init product interface
    
    * register create product
    
    * completing logic create handler
    
    * completing logic view product list

---
+++ internal/interfaces/http/api/v1/products/router.go
@@ -0,0 +1,40 @@
+package product
+
+import (
+	"net/http"
+
+	"github.com/resyahrial/go-commerce/config/app"
+	user_dom "github.com/resyahrial/go-commerce/internal/domains/user"
+	"github.com/resyahrial/go-commerce/internal/infrastructures"
+	"github.com/resyahrial/go-commerce/pkg/grest"
+)
+
+func Register(routes map[string]grest.Route) {
+	productUcase := infrastructures.InitProductUsecase(app.DB)
+	handler := New(productUcase)
+
+	grest.RegisterRoute(
+		routes,
+		"/products",
+		&grest.Route{
+			Path:       "/",
+			Method:     http.MethodPost,
+			Handler:    handler.Create,
+			IsNeedAuth: true,
+			Role:       user_dom.SELLER,
+		},
+		&grest.Route{
+			Path:       "/seller",
+			Method:     http.MethodGet,
+			Handler:    handler.ViewList,
+			IsNeedAuth: true,
+			Role:       user_dom.SELLER,
+		},
+		&grest.Route{
+			Path:       "/buyer",
+			Method:     http.MethodGet,
+			Handler:    handler.ViewList,
+			IsNeedAuth: true,
+		},
+	)
+}
