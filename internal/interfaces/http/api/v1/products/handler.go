275716baf0480ab9d9d3ce88d7ca36cfb1ed2a9f
resyahrial <syahrialrangga11@gmail.com>
2022-06-01 19:06:57 +0700

    completing logic create handler

--- internal/interfaces/http/api/v1/products/handler.go
+++ internal/interfaces/http/api/v1/products/handler.go
@@ -4,7 +4,10 @@ import (
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
+	product_dom "github.com/resyahrial/go-commerce/internal/domains/product"
 	product_ucase "github.com/resyahrial/go-commerce/internal/usecases/product"
+	"github.com/resyahrial/go-commerce/pkg/grest"
+	"github.com/resyahrial/go-commerce/pkg/gtrace"
 )
 
 type ProductHandlerInterface interface {
@@ -20,5 +23,23 @@ func New(productUcase product_ucase.ProductUsecaseInterface) ProductHandlerInter
 }
 
 func (h *ProductHandler) Create(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	var err error
+	var prod product_dom.Product
 
+	newCtx, span := gtrace.Start(r.Context())
+	defer gtrace.End(span, err)
+
+	if err = grest.ReadRequestBody(r, &prod); err != nil {
+		panic(err)
+	}
+
+	if prod, err = h.productUcase.Create(newCtx, prod); err != nil {
+		panic(err)
+	}
+
+	grest.WriteResponse(w, grest.Response{
+		Code:   http.StatusCreated,
+		Status: "Created",
+		Data:   prod,
+	})
 }
